9e53beaa9c62839cb61eefec4b9fb5b5d9b9f246
Hanzel Legarda <hlegarda@gmail.com>
2021-06-08 13:46:30 -0600

    removed old comments

--- Claws.py
+++ Claws.py
@@ -5,8 +5,5 @@ class Claws:
     claws_motor = Motor(Port.A)
 
     def move_claws(self):
-        #TODO calibrate
-        #claswMotor.run_angle(720, 90, Stop.BRAKE, True)
-        #claswMotor.run_angle(720, -90, Stop.BRAKE, True)
         self.claws_motor.run_until_stalled(720, Stop.BRAKE, 50)
         self.claws_motor.run_until_stalled(-720, Stop.BRAKE, 50)
